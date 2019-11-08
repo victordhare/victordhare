627d92242548379a5210562508220b3df5a7b5a7
victordhare <57521843+victordhare@users.noreply.github.com>
2019-11-08 00:21:40 -0800

    transport counter
    sqi project

---
+++ go.js
@@ -0,0 +1,104 @@
+function clickcounter(){
+	document.getElementById('car').style.display="block";
+}
+function ttcount(){
+	show.value=(++ show.value);
+
+}
+function ttcount1(){
+	show.value=(-- show.value);
+}
+function cler(){
+	show.value="";
+}
+
+function clickcounter1(){
+	document.getElementById('truck').style.display="block";
+}
+function ttcount2(){
+	show1.value=(++ show1.value);
+
+}
+function ttcount3(){
+	show1.value=(-- show1.value);
+}
+function cler1(){
+	show1.value="";
+}
+
+function clickcounter2(){
+	document.getElementById('tax').style.display="block"
+}
+function ttcount4(){
+	show2.value=(++ show2.value);
+
+}
+function ttcount5(){
+	show2.value=(-- show2.value);
+}
+function cler2(){
+	show2.value="";
+}
+
+function clickcounter3(){
+	document.getElementById('bikes').style.display="block"
+}
+function ttcount6(){
+	show3.value=(++ show3.value);
+
+}
+function ttcount7(){
+	show3.value=(-- show3.value);
+}
+function cler3(){
+	show3.value="";
+}
+
+function clickcounter4(){
+	document.getElementById('cycle').style.display="block"
+}
+function ttcount8(){
+	show4.value=(++ show4.value);
+
+}
+function ttcount9(){
+	show4.value=(-- show4.value);
+}
+function cler4(){
+	show4.value="";
+}
+
+function clickcounter5(){
+	document.getElementById('buses').style.display="block"
+}
+function ttcount10(){
+	show5.value=(++ show5.value);
+
+}
+function ttcount11(){
+	show5.value=(-- show5.value);
+}
+function cler5(){
+	show5.value="";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
